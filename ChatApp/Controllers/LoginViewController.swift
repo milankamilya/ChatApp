b4f7fd075adddde2966f6f420136c10f04e9235c
Alejandro Menendez <alejandro.menendez.pull@gmail.com>
2018-04-29 12:51:39 +0530

    Initial Push

---
+++ ChatApp/Controllers/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Menlo on 29/04/18.
+//  Copyright © 2018 self. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
